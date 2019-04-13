bb58c40dc89807736b5b8d3bd0d7ebef9233ade8
2018project <44795100+2018project@users.noreply.github.com>
2019-04-14 01:01:36 +0800

    yarn lock file removed

--- schema/mongosetting.js
+++ schema/mongosetting.js
@@ -9,7 +9,7 @@ const db = {
 }
 
 
-mongoose.connect("mongodb://yikyikyik:Aa123456@cluster0-shard-00-00-x7eeg.mongodb.net:27017,cluster0-shard-00-01-x7eeg.mongodb.net:27017,cluster0-shard-00-02-x7eeg.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true/gameshop")
+mongoose.connect("mongodb://yikyikyik:Aa123456@cluster0-shard-00-00-5rceq.mongodb.net:27017,cluster0-shard-00-01-5rceq.mongodb.net:27017,cluster0-shard-00-02-5rceq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true/gameshop")
 mongoose.Promise = global.Promise
 const Schema = mongoose.Schema
 
